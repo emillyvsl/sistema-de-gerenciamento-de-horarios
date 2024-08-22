15ba0011ecacbecc239155b0887b5582be364d9e
Emilly Silva <emilly.vitoria@sou.ufac.br>
2024-08-22 07:37:12 -0500

    conf inicial de autenticação

--- sgh_app/views.py
+++ sgh_app/views.py
@@ -1,3 +1,28 @@
+from sqlite3 import IntegrityError
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+def registro(request):
+    if request.method == 'GET':
+        return render(request, 'auth/cadastro.html')
+    else:
+        username = request.POST['nome']
+        password = request.POST['senha']
+        email = request.POST['email']
+
+        try:
+            # Verificar se o usuário já existe
+            user = User.objects.get(email=email)
+            return HttpResponse("Email já registrado.")
+        except User.DoesNotExist:
+            # Criar um novo usuário
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                return HttpResponse("Usuário registrado com sucesso!")
+            except IntegrityError:
+                return HttpResponse("Erro ao registrar o usuário.")
+
+
+def login(request):
+    return render(request, 'auth/login.html')
