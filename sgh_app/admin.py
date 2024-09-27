f7dd3fbde3a573812ae932212fbef403cc6f099a
sthefanySF <100035817+sthefanySF@users.noreply.github.com>
2024-09-26 19:57:25 -0500

    corrige lista de preferência

--- sgh_app/admin.py
+++ sgh_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from sgh_app.models.dias_semana import DiasSemana
 from sgh_app.models.horario_curso import HorarioCurso
+from sgh_app.models.preferencias import Preferencias
 from .models import Centro, Curso, Coordenacao, Periodo, Semestre
 
 class CursoAdmin(admin.ModelAdmin):
@@ -24,6 +25,18 @@ class HorarioCursoAdmin(admin.ModelAdmin): #deve ser retirado depois
     search_fields = ('curso__nome',)
     list_filter = ('curso',)
 
+
+class PreferenciasAdmin(admin.ModelAdmin):
+    list_display = ('professor', 'get_dias_preferidos', 'get_horas_preferidas')
+    
+    def get_dias_preferidos(self, obj):
+        return ', '.join([dia.nome for dia in obj.dias_preferidos.all()])
+    get_dias_preferidos.short_description = 'Dias Preferidos'
+    
+    def get_horas_preferidas(self, obj):
+        return ', '.join([hora.__str__() for hora in obj.horas_preferidas.all()])
+    get_horas_preferidas.short_description = 'Horas Preferidas'
+
 admin.site.register(Centro)
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(Coordenacao)
@@ -31,3 +44,4 @@ admin.site.register(Periodo, PeriodoAdmin)
 admin.site.register(Semestre)
 admin.site.register(DiasSemana, DiasSemanaAdmin)
 admin.site.register(HorarioCurso, HorarioCursoAdmin)
+admin.site.register(Preferencias, PreferenciasAdmin)
