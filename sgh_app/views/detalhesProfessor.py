0e76a1a8c4aa3aa70fd95a9215bbcab76cc43973
sthefanySF <100035817+sthefanySF@users.noreply.github.com>
2024-11-03 17:37:09 -0500

    Ajustes

--- sgh_app/views/detalhesProfessor.py
+++ sgh_app/views/detalhesProfessor.py
@@ -12,10 +12,14 @@ def detalhes_professor(request, professor_id):
     horarios_curso = HorarioCurso.objects.filter(curso=professor.curso)
     preferencias = professor.preferencias.all()  
 
+    # Verifique se há horários disponíveis
+    horarios_disponiveis = horarios_curso.exists()
+
     return render(request, 'detalhes_professor.html', {
         'professor': professor,
         'disciplinas': disciplinas,
         'dias_semana': dias_semana,
         'horarios_curso': horarios_curso,
         'preferencias': preferencias,  
+        'horarios_disponiveis': horarios_disponiveis, 
     })
